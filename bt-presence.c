097990ba76b18d91525fcf04724242c8ae4a8604
ImmineoII <eboccati@gmail.com>
2024-01-18 09:55:28 +0100

    logger: move to syslog

--- bt-presence.c
+++ bt-presence.c
@@ -6,6 +6,7 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
+#include <sys/syslog.h>
 
 int main(int argc, char **argv)
 {
@@ -15,10 +16,14 @@ int main(int argc, char **argv)
     char name[248] = { 0 };
     const char * mac_address_str = "C8:9F:0C:14:7D:33";
 
+    setlogmask(LOG_UPTO (LOG_DEBUG));
+    openlog("bt-presence.log", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
+    syslog(LOG_DEBUG, "starting bt-presence application");
+
     adapter_id = hci_get_route(NULL);
     sock = hci_open_dev( adapter_id );
     if (adapter_id < 0 || sock < 0) {
-        printf("error opening socket\n");
+        syslog(LOG_DEBUG, "error opening socket");
         exit(1);
     }
 
@@ -26,8 +31,9 @@ int main(int argc, char **argv)
     str2ba(mac_address_str, &bdaddr);
 
     while (1) {
+        memset(name, 0, sizeof(name));
         // use converted MAC to ask device name
-        printf("Asking for %s name\n", mac_address_str);
+        syslog(LOG_DEBUG, "asking for %s name", mac_address_str);
         if (hci_read_remote_name(sock, &bdaddr, sizeof(name), name, 0) < 0) {
             // device not found
             strcpy(name, "[unknown]");
@@ -36,7 +42,7 @@ int main(int argc, char **argv)
             // device found
             presence = true;
         }
-        printf("Device name: %s\n", name);
+        syslog(LOG_DEBUG, "device name: %s", name);
         sleep(1);
     }
 
